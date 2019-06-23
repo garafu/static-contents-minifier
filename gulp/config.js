0759eefe167590c106e3738cc350607e9e164989
Akinari Tsugo <garafo@gmail.com>
2019-06-24 00:45:23 +0900

    Remove closure-compiler options.

--- gulp/config.js
+++ gulp/config.js
@@ -3,14 +3,6 @@ module.exports = {
     input: "./src",
     output: "./dist"
   },
-  closure_compiler: {
-    compilation_level: "SIMPLE",
-    warning_level: "VERBOSE",
-    language_in: 'ECMASCRIPT6_STRICT',
-    language_out: 'ECMASCRIPT5_STRICT',
-    output_wrapper: '(function(){\n%output%\n}).call(this)',
-    js_output_file: 'output.min.js'
-  },
   uglify: {
 
   },
